2890dc23e3e9f045011f5f2404841ecf7f9331c3
Redundancia <hunyadiwalter94@gmail.com>
2021-09-23 01:45:09 +0200

    [add] contact entity, status enum

---
+++ src/main/java/hu/futureofmedia/task/contactsapi/entities/Contact.java
@@ -0,0 +1,48 @@
+package hu.futureofmedia.task.contactsapi.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    //regex if you want different restriction
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    //TODO regex the phone number ^\+?\d{10,14}$
+    private String phoneNumber;
+
+    @ManyToOne
+    @Column(nullable = false)
+    private Company company;
+
+    //TODO strip html tags in controller/service Jsoup.parse(html).text();
+    private String comment;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    private LocalDateTime creationDate;
+
+    private LocalDateTime lastUpdatedDate;
+}
