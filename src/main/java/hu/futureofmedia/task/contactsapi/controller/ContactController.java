c6ed0279773f98dc2c726aa82ee5b4daf9f37490
Redundancia <hunyadiwalter94@gmail.com>
2021-09-25 22:15:11 +0200

    [add] newContact endpoint

--- src/main/java/hu/futureofmedia/task/contactsapi/controller/ContactController.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/controller/ContactController.java
@@ -3,14 +3,12 @@ package hu.futureofmedia.task.contactsapi.controller;
 import hu.futureofmedia.task.contactsapi.entities.contact.Contact;
 import hu.futureofmedia.task.contactsapi.entities.contact.ContactDetailsDTO;
 import hu.futureofmedia.task.contactsapi.entities.contact.ContactFindAllActiveDTO;
+import hu.futureofmedia.task.contactsapi.entities.contact.ContactNewDTO;
 import hu.futureofmedia.task.contactsapi.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +33,11 @@ public class ContactController {
         Contact contact =contactService.findById(contactId).orElse(null);
         return contact == null ? new ResponseEntity<>(null, HttpStatus.OK) :
                 new ResponseEntity<>(new ContactDetailsDTO(contact), HttpStatus.OK);
+    }
+
+    @PostMapping("/new")
+    @ResponseBody
+    public ResponseEntity<String> addNewContact(@RequestBody ContactNewDTO contactNewDTO) {
+        return contactService.validateAndSave(contactNewDTO);
     }
 }
