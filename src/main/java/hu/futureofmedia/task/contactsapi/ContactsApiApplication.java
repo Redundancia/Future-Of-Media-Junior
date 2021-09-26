badb5e20e62a0719e056ee2c2167e0d5466f66f2
Redundancia <hunyadiwalter94@gmail.com>
2021-09-26 17:07:20 +0200

    [add] add some api details

--- src/main/java/hu/futureofmedia/task/contactsapi/ContactsApiApplication.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/ContactsApiApplication.java
@@ -2,13 +2,30 @@ package hu.futureofmedia.task.contactsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 @EnableSwagger2
 public class ContactsApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ContactsApiApplication.class, args);
+    }
+
+    @Bean
+    public Docket swaggerConfiguration() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("hu.futureofmedia.task.contactsapi.controller"))
+                .build()
+                .apiInfo(new ApiInfoBuilder().title("Contact API").build());
     }
 }
