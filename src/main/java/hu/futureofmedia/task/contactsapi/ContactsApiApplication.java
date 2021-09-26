296780f8179c0c80417a0f2f09eee3dca9b0e7b5
Redundancia <hunyadiwalter94@gmail.com>
2021-09-26 16:09:57 +0200

    [add] enable swagger

--- src/main/java/hu/futureofmedia/task/contactsapi/ContactsApiApplication.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/ContactsApiApplication.java
@@ -2,8 +2,10 @@ package hu.futureofmedia.task.contactsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ContactsApiApplication {
 
     public static void main(String[] args) {
